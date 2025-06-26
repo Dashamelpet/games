c8db3cc4f6c084d689a8b8cc46bfc206460b0603
Daria <dashapetrash@gmail.com>
2025-06-26 17:32:11 +0200

    v1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let popularGamesSwiper = new Swiper(".popularGamesSwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    pagination: {
+      clickable: true,
+    },
+  });
+let newGames = new Swiper(".newGamesSwiper", {
+    slidesPerView: "auto",
+    pagination: {
+      clickable: true,
+    },
+ 
+  });
